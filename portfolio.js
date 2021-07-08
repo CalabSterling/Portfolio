c26e478c8538a7ce139357be53df895925008981
Calab Sterling <calabsterling@gmail.com>
2021-07-07 23:49:33 -0400

    first commit

---
+++ portfolio.js
@@ -0,0 +1,6 @@
+let carousel1 = document.querySelector("#carousel1");
+let carousel2 = document.querySelector("#carousel2");
+
+if (screen.width < "640px") {
+    carousel2.style = "visibility: null";
+}
